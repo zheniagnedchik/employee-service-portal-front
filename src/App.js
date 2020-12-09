6f644b8bfc64a839958a0515a70985ad34eee82a
bigory <igorboldysh@gmail.com>
2020-12-10 01:02:44 +0300

    add interveiw

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+
+  const [appState, setAppState] = useState({question:[]});
+  useEffect(() => {
+    const apiUrl = 'https://cors-anywhere.herokuapp.com/https://portal-employee-service.herokuapp.com/interview';
+    axios.get(apiUrl).then((resp) => {
+      const question = resp.data;
+      setAppState({test:question});
+    });
+  }, [setAppState]);
+  console.log(appState)
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
